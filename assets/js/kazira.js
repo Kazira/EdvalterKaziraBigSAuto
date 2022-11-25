be12fbe6bc65de4cec028747925ba81159786916
Edvalter da Costa <edvalterjamba9@gmail.com>
2022-11-25 16:46:19 +0200

    Add files via upload

---
+++ assets/js/kazira.js
@@ -0,0 +1,37 @@
+/*****************************************************************************************************************
+Done By
+Edvalter da Costa Jamba || Kazira©
+PhoneNumber:0845385688
+Email: kazirainfo@gmail.com
+It was agreed that the name of my Start-Up Kazira© would be in the footer of the website 
+for the Company named Big S Auto Trader and no change should be made. 
+******************************************************************************************************************/
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
